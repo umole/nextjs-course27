e80f2de7ca080a51c751fc8321fda0bc6dc78191
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-05-02 23:39:37 +0100

    creating a new nextjs poject

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
